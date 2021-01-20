730983cbac16813418559b677cd4fabe5000dfea
Shane Jeremich <shane.jeremich@gmail.com>
2021-01-19 18:44:59 -0700

    finished adding gradient animation for hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const btnHamburger = document.getElementById("btn-hamburger"),
+  header = document.querySelector(".header"),
+  overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener("click", () => {
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
+});
