668319f019c9a0889d9300be2b68eb675c970292
Shane Jeremich <shane.jeremich@gmail.com>
2021-01-20 01:04:08 -0700

    finished add mobile menu and mobile design

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,23 @@
 const btnHamburger = document.getElementById("btn-hamburger"),
+  body = document.querySelector("body"),
   header = document.querySelector(".header"),
-  overlay = document.querySelector(".overlay");
+  overlay = document.querySelector(".overlay"),
+  fadeElements = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElements.forEach(element => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
+    body.classList.add("noscroll");
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElements.forEach(element => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
